317f2e8e6d55e4ff85a06d22540e7b97dbd73969
Nir Azkiel <nir.azkiel@realsenseai.com>
2026-05-24 19:22:26 +0300

    Rebase from 'rpm/humble/realsense2_camera'

--- include/constants.h
+++ include/constants.h
@@ -18,8 +18,8 @@
 #include "ros_node_base.h"
 
 #define REALSENSE_ROS_MAJOR_VERSION    4
-#define REALSENSE_ROS_MINOR_VERSION    57
-#define REALSENSE_ROS_PATCH_VERSION    7
+#define REALSENSE_ROS_MINOR_VERSION    58
+#define REALSENSE_ROS_PATCH_VERSION    1
 
 #define STRINGIFY(arg) #arg
 #define VAR_ARG_STRING(arg) STRINGIFY(arg)
